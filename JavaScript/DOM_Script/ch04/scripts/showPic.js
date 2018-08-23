ac0ffd14388123fa253e2236258dec6c07609842
WangJian <wangluojisuan@163.com>
2018-08-23 10:12:15 +0800

    添加说明

--- JavaScript/DOM_Script/ch04/scripts/showPic.js
+++ JavaScript/DOM_Script/ch04/scripts/showPic.js
@@ -1,5 +1,16 @@
 function showPic(whichPic) {
     var source = whichPic.getAttribute("href");
+    var text = whichPic.getAttribute("title");
     var placeholder = document.getElementById("placeholder");
     placeholder.setAttribute("src", source);
+    var description = document.getElementById("description");
+    //alert(description.childNodes[0].nodeValue);
+    description.firstChild.nodeValue = text;
 }
+
+function countBodyChildren() {
+    var body_element = document.getElementsByTagName("body")[0];
+    //alert(body_element.nodeType)
+}
+
+window.onload = countBodyChildren();
